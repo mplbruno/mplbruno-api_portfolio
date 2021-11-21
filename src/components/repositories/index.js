91c71bdfb4c76fa68a5cd86b258e519c4e4e6ef9
mplbruno <mplbruno@gmail.com>
2021-11-21 12:00:54 -0300

    Criei as seguintes pastas e arquivos src/components/repository-item/index.js, src/hooks/github-hooks.js e src/providers/github-provider.js. Modifiquei os segintes arquivos src/App.js, src/components/profile/index.js e src/components/repositories/index.js

--- src/components/repositories/index.js
+++ src/components/repositories/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import * as S from "./styled";
+import RepositoryItem from "../repository-item";
 
 const Repositories = () => {
     return (
@@ -11,8 +12,21 @@ const Repositories = () => {
                 <S.WrapperTab>Repositories</S.WrapperTab>
                 <S.WrapperTab>Starred</S.WrapperTab>                  
             </S.WrapperTabList>
-            <S.WrapperTabPanel>panel Repositories</S.WrapperTabPanel>
-            <S.WrapperTabPanel>panel Starred</S.WrapperTabPanel>
+            <S.WrapperTabPanel>
+            <RepositoryItem 
+            name="apiCatalogoJogos" 
+            linkToRepo="https://github.com/mplbruno/apiCatalogoJogos"
+            fullName="mplbruno/apiCatalogoJogos" 
+                
+                />
+            </S.WrapperTabPanel>
+            <S.WrapperTabPanel>
+            <RepositoryItem 
+                name="Aulas" 
+                linkToRepo="https://github.com/mplbruno/Aulas"
+                fullName="mplbruno/Aulas"
+             />
+            </S.WrapperTabPanel>
 
     </S.WrapperTabs>
     );
